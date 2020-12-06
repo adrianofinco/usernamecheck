c9307191d1d6bba5c0e90f5e6c96c9c4b125096b
adrianofinco <adriano.finco.509@gmail.com>
2020-12-06 17:58:54 -0300

    fix: typo in example

--- examples/1-singular.js
+++ examples/1-singular.js
@@ -3,8 +3,8 @@ const { listWebsites, isUsernameAvailable } = require('../src/usernamecheck')
 let websiteList = listWebsites()
 console.log(websiteList)
 
-// isUsernameAvailable is asynchronously you need to use .then to acces the value
-isUsernameAvailable('octocat', 'github').then(val => { console.log(val) })
+// isUsernameAvailable is asynchronous you need to use .then to acces the value
+isUsernameAvailable('octocat', 'reddit').then(val => { console.log(val) })
 
 
 
